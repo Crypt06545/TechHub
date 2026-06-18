70336586f018edc3537336d7c9778325878851a5
Crypt06545 <75322830+Crypt06545@users.noreply.github.com>
2026-06-18 08:11:18 +0600

    remove the port for render host

--- server/app.js
+++ server/app.js
@@ -18,12 +18,10 @@ dotenv.config();
 const app = express();
 
 // 1. Global security & CORS middlewares
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
-  }),
-);
+cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+});
 app.use(cookieParser());
 app.use(morgan("dev")); // Added "dev" format string to fix morgan
 app.use(
